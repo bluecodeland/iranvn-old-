58b6ec2d5dac5e882b7c7edfcf6467017f0547b4
bluecodeland <mhtaeb@gmail.com>
2017-12-28 03:11:18 +0330

    Revert "404"
    This reverts commit 073154972d9429062dd7416a2fe1e144d18a472c.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+{{--  <h2>{{ $exception->getMessage() }}</h2>  --}}
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<h2 class="text-center">صفحه مورد نظر یافت نشد. </h2>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,33 @@
+<<<<<<< HEAD
 <!doctype html>
+=======
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+{{--  <!doctype html>
+>>>>>>> parent of 0731549... 404
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
