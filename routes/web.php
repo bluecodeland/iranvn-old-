ce95f835f8d8c1493849033d8d2f4082da2172a3
bluecodeland <mhtaeb@gmail.com>
2017-12-29 17:00:59 +0330

    adding chat

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/chat', function () {
+    return view('chat');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
