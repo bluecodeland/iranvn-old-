49283cab0341f9d5ac6c778a2315bbb16308add6
bluecodeland <mhtaeb@gmail.com>
2018-10-26 13:46:44 +0330

    adfadf
    a

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -104,7 +104,7 @@
           socket.emit('new note', {note: newNote});
           $('#m').val('');
           
-          window.scrollTo(0,document.body.scrollHeight + 50);
+          window.scrollTo(0, 500000);
           
           
       })
@@ -113,7 +113,7 @@
         var newNote =  $('#m').val() + ': {{ Auth::user()->name }}   ';
             socket.emit('new note', {note: newNote});
             $('#m').val('');
-            window.scrollTo(0,document.body.scrollHeight + 50);}
+            window.scrollTo(0, 500000);}
             
         })
   
