3750ada5ddc37f30e28bda1d87486d09b4101f44
bluecodeland <mhtaeb@gmail.com>
2018-10-27 13:20:41 +0330

    lkl;klk

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,7 +5,7 @@
 <span id="typing"></span>
 <div class="container text-right">
   <div id="usersConnected" class="bg-primary"></div>
-  <div id="notes" style="min-height:50vh; max-height:50vh; height:50vh; overflow-y: scroll;"></div>
+  <div id="notes" style="min-height:70vh; max-height:70vh; height:70vh; overflow-y: scroll;"></div>
   <div ></div>
   <div style="position:fixed; bottom:0;">
 
